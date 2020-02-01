ce00d1a325189f8229f1f80e43d3188441811dcd
inoledge <info@inoledge.net>
2020-02-01 20:22:57 +0200

    updates-profule

--- resources/views/client/pages/inc/academy/profile.blade.php
+++ resources/views/client/pages/inc/academy/profile.blade.php
@@ -1,4 +1,4 @@
-<div class="alice-bg padding-top-60 section-padding-bottom">
+<div class="alice-bg padding-top-60 section-padding-bottom " id="profile">
       <div class="container">
         <div class="row">
           <div class="col">
@@ -9,7 +9,7 @@
                     <img src="{{url('custom')}}/images/inoledge.jpg" class="img-fluid" alt="">
                   </div>
                   <div class="title-body">
-                    <h4>Inoledge</h4>
+                    <h1>Inoledge <span class="ver-badge"><i class="fas fa-check-circle" title="Verified"></i></span></h1>
                     <div class="info">
                       <span class="candidate-designation"><i data-feather="check-square"></i>Software Training</span>
                     
@@ -17,14 +17,14 @@
                   </div>
                 </div>
                 <div class="download-resume">
-                  <a href="#">Download CV <i data-feather="download"></i></a>
+                  <a href="#">Apply Now</a>
                 </div>
               </div>
               <div class="skill-and-profile">
                 <div class="skill">
-                  <label>Skills:</label>
-                  <a href="#">Design</a>
-                  <a href="#">Illustration</a>
+                  <label>Keywords:</label>
+                  <a href="#">Software</a>
+                  <a href="#">Programming</a>
                   <a href="#">iOS</a>
                 </div>
                 <div class="social-profile">
@@ -42,166 +42,42 @@
               </div>
               <div class="branches-box">
                       <i data-feather="map-pin"></i> Branches
-                          <i class="branch-title">Giza</i>
-                          <i class="branch-title">Alexandria</i>
-                          <i class="branch-title">Alexandria</i>
-                          <i class="branch-title">Alexandria</i>
-                          <i class="branch-title">Alexandria</i>
-                          <i class="branch-title">Alexandria</i>
-                          <i class="branch-title">Alexandria</i>
+                        <a href="#">  <i class="branch-title">Giza</i></a>
+                        <a href="#">  <i class="branch-title">Alexandria</i></a>
               </div>
               <div class="details-information section-padding-60">
                 <div class="row">
                   <div class="col-xl-7 col-lg-8">
-                    <div class="about-details details-section">
-                      <h4><i data-feather="align-left"></i>About Me</h4>
-                      <p>Combined with a handful of model sentence structures, to generate lorem Ipsum which  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including.</p>
-                      <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable </p>
+                  <!--  Tabs !-->
+                  <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                      <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">About</a>
+                      <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+                      <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
                     </div>
-                    <div class="edication-background details-section">
-                      <h4><i data-feather="book"></i>Education Background</h4>
-                      <div class="education-label">
-                        <span class="study-year">2018 - Present</span>
-                        <h5>Masters in Software Engineering<span>@ Engineering University</span></h5>
-                        <p>Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
-                      </div>
-                      <div class="education-label">
-                        <span class="study-year">2014 - 2018</span>
-                        <h5>Diploma in Graphics Design<span>@ Graphic Arts Institute</span></h5>
-                        <p>Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
-                      </div>
-                      <div class="education-label">
-                        <span class="study-year">2008 - 2014</span>
-                        <h5>Secondary School Certificate<span>@  Engineering High School</span></h5>
-                        <p>Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
-                      </div>
-                    </div>
-                    <div class="experience details-section">
-                      <h4><i data-feather="briefcase"></i>Work Experiance</h4>
-                      <div class="experience-section">
-                        <span class="service-year">2016 - Present</span>
-                        <h5>Lead UI/UX Designer<span>@ Codepassengers LTD</span></h5>
-                        <p>Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
-                      </div>
-                      <div class="experience-section">
-                        <span class="service-year">2012 - 2016</span>
-                        <h5>Former Graphic Designer<span>@ Graphicreeeo CO</span></h5>
-                        <p>Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage</p>
-                      </div>
-                    </div>
-                    <div class="professonal-skill details-section">
-                      <h4><i data-feather="feather"></i>Professional Skill</h4>
-                      <p>Combined with a handful of model sentence structures, to generate lorem Ipsum which  It has survived not only five centuries, but also the leap into electronic typesetting</p>
-                      <div class="progress-group">
-                        <div class="progress-item">
-                          <div class="progress-head">
-                            <p class="progress-on">Photoshop</p>
-                          </div>
-                          <div class="progress-body">
-                            <div class="progress">
-                              <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 0;"></div>
+                  </nav>
+                  <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active profile-tab-content" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                            <div class="about-details details-section">
+                                        
+                                        <p>Combined with a handful of model sentence structures, to generate lorem Ipsum which  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including.</p>
+                                        <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable </p>
                             </div>
-                            <p class="progress-to">70%</p>
-                          </div>
-                        </div>
-                        <div class="progress-item">
-                          <div class="progress-head">
-                            <p class="progress-on">HTML/CSS</p>
-                          </div>
-                          <div class="progress-body">
-                            <div class="progress">
-                              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 0;"></div>
-                            </div>
-                            <p class="progress-to">90%</p>
-                          </div>
-                        </div>
-                        <div class="progress-item">
-                          <div class="progress-head">
-                            <p class="progress-on">JavaScript</p>
-                          </div>
-                          <div class="progress-body">
-                            <div class="progress">
-                              <div class="progress-bar" role="progressbar" aria-valuenow="74" aria-valuemin="0" aria-valuemax="100" style="width: 0;"></div>
-                            </div>
-                            <p class="progress-to">74%</p>
-                          </div>
-                        </div>
-                        <div class="progress-item">
-                          <div class="progress-head">
-                            <p class="progress-on">PHP</p>
-                          </div>
-                          <div class="progress-body">
-                            <div class="progress">
-                              <div class="progress-bar" role="progressbar" aria-valuenow="86" aria-valuemin="0" aria-valuemax="100" style="width: 0;"></div>
-                            </div>
-                            <p class="progress-to">86%</p>
-                          </div>
-                        </div>
-                      </div>
                     </div>
-                    <div class="special-qualification details-section">
-                      <h4><i data-feather="gift"></i>Special Qualification</h4>
-                      <ul>
-                        <li>5 years+ experience designing and building products.</li>
-                        <li>Skilled at any Kind Design Tools.</li>
-                        <li>Passion for people-centered design, solid intuition.</li>
-                        <li>Hard Worker & Quick Lerner.</li>
-                      </ul>
-                    </div>
-                    <div class="portfolio details-section">
-                      <h4><i data-feather="gift"></i>Portfolio</h4>
-                      <div class="portfolio-slider owl-carousel">
-                        <div class="portfolio-item">
-                          <img src="images/portfolio/thumb-3.jpg" class="img-fluid" alt="">
-                          <div class="overlay">
-                            <a href="#"><i data-feather="eye"></i></a>
-                            <a href="#"><i data-feather="link"></i></a>
-                          </div>
-                        </div>
-                        <div class="portfolio-item">
-                          <img src="images/portfolio/thumb-1.jpg" class="img-fluid" alt="">
-                          <div class="overlay">
-                            <a href="#"><i data-feather="eye"></i></a>
-                            <a href="#"><i data-feather="link"></i></a>
-                          </div>
-                        </div>
-                        <div class="portfolio-item">
-                          <img src="images/portfolio/thumb-2.jpg" class="img-fluid" alt="">
-                          <div class="overlay">
-                            <a href="#"><i data-feather="eye"></i></a>
-                            <a href="#"><i data-feather="link"></i></a>
-                          </div>
-                        </div>
-                        <div class="portfolio-item">
-                          <img src="images/portfolio/thumb-3.jpg" class="img-fluid" alt="">
-                          <div class="overlay">
-                            <a href="#"><i data-feather="eye"></i></a>
-                            <a href="#"><i data-feather="link"></i></a>
-                          </div>
-                        </div>
-                        <div class="portfolio-item">
-                          <img src="images/portfolio/thumb-2.jpg" class="img-fluid" alt="">
-                          <div class="overlay">
-                            <a href="#"><i data-feather="eye"></i></a>
-                            <a href="#"><i data-feather="link"></i></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                    <div class="tab-pane fade profile-tab-content" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                    <div class="tab-pane fade profile-tab-content" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+                  </div>
+                  <!--  End Tabs !-->
+                 
                   </div>
                   <div class="col-xl-4 offset-xl-1 col-lg-4">
                     <div class="information-and-contact">
                       <div class="information">
                         <h4>Information</h4>
                         <ul>
-                          <li><span>Category:</span> Design & Creative</li>
-                          <li><span>Location:</span> Los Angeles</li>
-                          <li><span>Status:</span> Full-time</li>
-                          <li><span>Experience:</span> 3 year(s)</li>
-                          <li><span>Salary:</span> $32k - $36k</li>
-                          <li><span>Gender:</span> Male</li>
-                          <li><span>Age:</span> 24 Year(s)</li>
-                          <li><span>Qualification:</span> Gradute</li>
+                          <li><span>Founded:</span>2012</li>
+                          <li><span>Location:</span> Giza,Egypt</li>
+                          <li><span>Training Students:</span> 8620</li>
                         </ul>
                       </div>
                       <div class="buttons">
@@ -238,7 +114,7 @@
               <div class="row">
                 <div class="col-xl-7 col-lg-8">
                   <div class="personal-information details-section">
-                    <h4><i data-feather="user-plus"></i>Personal Deatils</h4>
+                    <h4><i data-feather="user-plus"></i>Reviews</h4>
                     <ul>
                       <li><span>Full Name:</span> Micheal N. Taylor</li>
                       <li><span>Father's Name:</span> Howard Armour</li>
